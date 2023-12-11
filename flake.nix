b8492e7c67c55afd5191848eceb518f198aa3556
muhchaudhary <muhammadahmchaudhary@gmail.com>
2023-12-11 17:15:07 -0500

    change pname

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
         };
   in {
     overlays.default = final: prev: {
-      aylurs-dots_1 = mkAgsDots {
+      aylurs-dots = mkAgsDots {
         pname = "aylurs-dots";
         version = "1";
       };
@@ -42,9 +42,9 @@
     packages.x86_64-linux = rec {
       inherit
         (pkgs)
-        aylurs-dots_1
+        aylurs-dots
         ;
-      default = aylurs-dots_1;
+      default = aylurs-dots;
     };
   };
 }

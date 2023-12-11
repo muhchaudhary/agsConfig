69668c1fcd551c06f4962d61c25f9b2041125492
muhchaudhary <muhammadahmchaudhary@gmail.com>
2023-12-11 16:54:26 -0500

    update test message

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           inherit pname version;
 
           buildCommand = ''
-            mkdir -p $out && ls && cp -r ./src/* $out
+            mkdir -p $out && ls -altr && cp -r ./src/* $out
           '';
         };
   in {

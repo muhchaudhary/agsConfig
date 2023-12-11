82958d2b0f1cefb7d6117cc6c0d67130c7f45128
muhchaudhary <muhammadahmchaudhary@gmail.com>
2023-12-11 15:39:39 -0500

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Aylurs default config files for ags";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    self,
+  }: let
+    genSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+    pkgs = genSystems (system: import nixpkgs {inherit system;});
+  in {
+    packages = genSystems (system: {
+      default = pkgs.${system}.callPackage ./nix {};
+    });
+  };
+}

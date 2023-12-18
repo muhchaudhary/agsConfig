2aaf2cc6054cd2e531932883c492326f5295b7a9
muhchaudhary <muhammadahmchaudhary@gmail.com>
2023-12-17 21:07:28 -0500

    formatting

--- ags-config/js/bar/buttons/PayerTimes.js
+++ ags-config/js/bar/buttons/PayerTimes.js
@@ -1,32 +1,34 @@
-import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-import prayerService from "../../services/PrayerTimesService.js";
-import PanelButton from '../PanelButton.js';
+import Widget from 'resource:///com/github/Aylur/ags/widget.js'
+import prayerService from '../../services/PrayerTimesService.js'
+import PanelButton from '../PanelButton.js'
 
-export default () => PanelButton({
+export default () =>
+  PanelButton({
     class_name: 'bar-prayer-times-box panel-button',
     content: Widget.Label({
-        truncate: 'end',
-        xalign: 0,
-        maxWidthChars: 24,
-        label: '',
-        connections: [
-            [
-            prayerService,
-            self => {
-                        {
-                            if (prayerService.nextPrayerName != '') {
-                                if (!prayerService.prayerNow) {
-                                    self.label = `${prayerService.nextPrayerName}` + 
-                                                `(${prayerService.nextPrayerTime})`
-                                } else {
-                                    self.label = `حان الان وقت صلاة ${prayerService.prayerNow}`
-                                }
-                            } else {
-                                self.label = `غير متاحة`;
-                            }
-                        }
+      truncate: 'end',
+      xalign: 0,
+      maxWidthChars: 24,
+      label: '',
+      connections: [
+        [
+          prayerService,
+          self => {
+            {
+              if (prayerService.nextPrayerName != '') {
+                if (!prayerService.prayerNow) {
+                  self.label =
+                    `${prayerService.nextPrayerName}` +
+                    `(${prayerService.nextPrayerTime})`
+                } else {
+                  self.label = `حان الان وقت صلاة ${prayerService.prayerNow}`
                 }
-            ],
-        ],
-    }),
-});
+              } else {
+                self.label = `غير متاحة`
+              }
+            }
+          }
+        ]
+      ]
+    })
+  })
